159ebf934f648161b13050a142ef7c6691def266
zzy <2744726697@qq.com>
2024-08-24 18:17:07 +0800

    可视化组件中图表的拆分

---
+++ src/views/Agriculture/DiseaseVisual/components/Chart1.vue
@@ -0,0 +1,215 @@
+<template>
+  <!--part1-->
+    <div class="min-h-72 sm:h-full md:w-2/5 w-full">
+        <div class="h-fit w-full mb-1 md:mb-0 flex items-center">
+            <!--标题-->
+            <n-text class="font-thin text-2xl">平台数据总览</n-text>
+
+            <!--切换按钮-->
+            <n-text class="text-xl ms-auto text-sky-600 flex items-center cursor-pointer hover:text-sky-900 duration-1000">
+                <n-icon
+                        class="ms-auto text-xl"
+                        :component="WindStream"
+                ></n-icon>
+                切换为实时数据
+            </n-text>
+
+        </div>
+
+        <!--chart1 -->
+        <n-card
+                title=" "
+                class="h-[90%] w-full p-0"
+                content-class="h-full w-full p-2 pt-0"
+                header-class="p-1 pr-2"
+                header-extra-class="w-full ps-2"
+                footer-class="font-thin text-zinc-400"
+        >
+
+            <template #header-extra>
+
+                <!--title占位符-->
+                <span v-show="loading" class="animate-ping bg-red-200 rounded-full w-2 h-2 opacity-75"></span>
+                <n-text class="ms-3" v-show="loading">
+                    正在加载中......
+                </n-text>
+                <n-skeleton v-show="loading" class="w-20 h-5 ms-auto" round/>
+
+                <!--title内容-->
+                <n-switch v-show="!loading" class="ms-auto" @update:value="switchMode">
+                    <template #checked>
+                        按月显示
+                    </template>
+                    <template #unchecked>
+                        按周显示
+                    </template>
+                </n-switch>
+            </template>
+            <!--chart1占位符-->
+            <n-space
+                    v-show="loading"
+                    vertical class="w-full h-full"
+                    :wrap-item="false"
+                    :wrap="false"
+            >
+                <n-skeleton class="w-full h-1/6" :sharp="false"/>
+                <n-skeleton class="w-full h-1/6" :sharp="false"/>
+                <n-skeleton class="w-full h-1/6" :sharp="false"/>
+                <n-skeleton class="w-full h-1/6" :sharp="false"/>
+                <n-skeleton class="w-full h-1/6" :sharp="false"/>
+                <n-skeleton class="w-full h-1/6" :sharp="false"/>
+            </n-space>
+
+            <!--chart1内容-->
+            <div v-show="!loading" ref="chartDom" class="w-full h-full"></div>
+
+            <template #footer>上次更新于{{ dateTime }}</template>
+
+        </n-card>
+
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import {ref, reactive, onMounted, nextTick} from "vue";
+import {WindStream} from "@vicons/carbon";
+import * as echarts from "echarts/core";
+import {getAllDiseaseCategory, getDisVisual} from "@/apis/disVisual.js";
+import {genDateTime} from "@/utils/genDateTime.js";
+
+
+// 图表dom元素
+const chartDom = ref<HTMLDivElement | null>(null)
+// 图表实例对象
+const myChart = ref<EChartsType | null>(null)
+const loading = ref(true)
+const checked = ref(false)
+// 病害分类及其详情数据
+const disCategory = ref([])
+// 由病害名称组成的列表
+const disNames = ref([])
+// 用于展示最后一次数据更新的时间
+const dateTime = ref("XXXX-XX-XX")
+// 读取store中主题配置
+import {useDesignSettingStore} from "@/stores/designSetting.js";
+import {storeToRefs} from "pinia";
+import {EChartsType} from 'echarts'
+
+const themeStore = useDesignSettingStore()
+const {theme} = storeToRefs(themeStore)
+
+
+const switchMode = (value) => {
+    // value默认false也就是周, 可以换成month
+    console.log(value)
+    if (value) {
+        // 表示从周切换到月
+        updateChart("month")
+    } else {
+        updateChart("week")
+    }
+    checked.value = !checked.value
+
+}
+const option = reactive({
+    legend: {
+        data: ['玉米叶斑病', '玉米锈病', '玉米叶枯病', '玉米条纹病', '玉米黑粉病']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周天']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: '玉米叶斑病',
+            type: 'line',
+            stack: 'Total',
+            data: [120, 132, 101, 134, 90, 230, 210]
+        }
+    ]
+})
+
+/**
+ * 用于绘制以month/week为分组的图表.
+ * @param mode 可以是week或month
+ * @param chartDom 图表的dom对象
+ */
+async function initChart(mode, chartDom) {
+    // 提前取消加载状态,否则echarts无法及时获取元素宽高造成渲染图表过小的bug
+    loading.value = false
+    await updateChart(mode)
+    // 初始化操作晚一些执行,等待dom加载完成
+    myChart.value = echarts.init(chartDom, "dark");
+    option && myChart.value.setOption(option);
+    return myChart
+}
+
+/**
+ * 更新图表数据
+ * @param mode 可可选week,month
+ * @return {Promise<void>}
+ */
+async function updateChart(mode) {
+    // 请求数据
+    const resp = await getDisVisual(mode)
+    // 设置标签
+    option.legend.data = disNames.value
+    // 设置x轴数据
+    option.xAxis.data = resp[mode]
+    // 设置y轴数据
+    option.series = Object.keys(resp.datas).map(key => ({
+        name: key,
+        type: 'line',
+        stack: 'Total',
+        data: resp.datas[key]
+    }))
+
+    // 添加没用出现的病害类别， 并初始化data为全是0的数组
+    for (let disName of disNames.value) {
+        option.series.push(
+            {
+                name: disName,
+                type: 'line',
+                stack: 'Total',
+                data: [0, 0, 0, 0, 0, 0, 0]
+            }
+        )
+    }
+    myChart.value?.setOption(option)
+    // 记录数据更新时间
+    dateTime.value = genDateTime()
+
+}
+
+onMounted(async () => {
+    disCategory.value = await getAllDiseaseCategory()
+    disNames.value = disCategory.value.map(value => {
+        return value.name
+    })
+    await nextTick(async () => {
+        await initChart("week", chartDom.value)
+    })
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Agriculture/DiseaseVisual/components/Chart2.vue
+++ src/views/Agriculture/DiseaseVisual/components/Chart2.vue
@@ -1,25 +1,26 @@
 <template>
-    <n-layout
-            :native-scrollbar="false"
-            class="p-[20px]"
-            ref="containerRef"
-            id="visualContainer"
-            content-class="w-full h-full"
-    >
-        <!--内容容器-->
-        <n-space class="w-full h-full p-0 md:flex-nowrap flex-wrap" size="small" :wrap-item="false">
-            <!--part1-->
-            <Chart1></Chart1>
-
-            <!--part2-->
-            <Chart2></Chart2>
+    <n-card class="min-h-72 sm:h-full md:w-3/5 w-full m-0 p-0" content-class="h-full w-full p-2">
+        <!--chart2-->
+        <div v-show="!loading" ref="chartDom" class="h-full w-full"></div>
+        <!--chart2占位符-->
+        <n-space v-show="loading" class="w-full h-full" align="end" :size="9"
+                 :wrap-item="false" :wrap="false">
+            <n-skeleton class="w-[10%] h-[40%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[60%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[30%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[70%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[50%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[90%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[40%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[60%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[80%]" :sharp="false"/>
+            <n-skeleton class="w-[10%] h-[100%]" :sharp="false"/>
         </n-space>
-
-    </n-layout>
+    </n-card>
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue'
+import {ref, onMounted, reactive} from "vue";
 import * as echarts from 'echarts/core';
 import {
     TitleComponent,
@@ -31,24 +32,10 @@ import {
 import {LineChart, BarChart} from 'echarts/charts';
 import {UniversalTransition, LabelLayout} from 'echarts/features';
 import {CanvasRenderer} from 'echarts/renderers';
-
 import {getDisVisual} from "@/apis/disVisual.js";
-import Chart1 from "@/views/Agriculture/DiseaseVisual/components/Chart1.vue";
-import Chart2 from "@/views/Agriculture/DiseaseVisual/components/Chart2.vue";
-
 
 const loading = ref(true)
-// 本组件内容根容器, 用于检测此区域大小改变以使其图表自适应
-const containerRef = ref(null)
-// 病害分类及其详情数据
-const disCategory = ref([])
-// 由病害名称组成的列表
-const disNames = ref([])
-
-// 用于展示最后一次数据更新的时间
-const dateTime = ref("XXXX-XX-XX")
-
-
+const myChart = ref(null)
 // 有关图表2的额外配置-----------------
 const app = {};
 const posList = [
@@ -128,6 +115,7 @@ app.config = {
         });
     }
 };
+const chartDom = ref(null)
 const labelOption = {
     show: true,
     position: app.config.position,
@@ -141,6 +129,43 @@ const labelOption = {
         name: {}
     }
 };
+const option = reactive({
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    legend: {
+        data: ['叶斑病', '锈病', '叶枯病', '条纹病', '黑粉病']
+    },
+    toolbox: {
+        show: true,
+        orient: 'vertical',
+        left: 'right',
+        top: 'center',
+        feature: {
+            mark: {show: true},
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar', 'stack']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+        }
+    },
+    xAxis: [
+        {
+            type: 'category',
+            axisTick: {show: false},
+            data: ['2024', '2025', '2026', '2027', '2028']
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series: []
+});
 //-----------------echarts使用的插件------------------------------------------
 echarts.use([
     TitleComponent,
@@ -155,38 +180,43 @@ echarts.use([
     LabelLayout
 ]);
 
+/**
+ * 用于绘制以年份分组的图表
+ * @return {Promise<EChartsType>}
+ */
+async function chartYear() {
+
+    loading.value = false
+    // 请求数据
+    const resp = await getDisVisual("year")
 
 
-
+    // 设置标签
+    option.legend.data = Object.keys(resp.datas)
+    // 设置x轴数据
+    option.xAxis[0].data = resp.year
+    // 设置y轴数据
+    option.series = Object.keys(resp.datas).map(key => ({
+        name: key,
+        type: 'bar',
+        barGap: 0,
+        label: labelOption,  // 使用 labelOption 配置数据标签
+        emphasis: {
+            focus: 'series'
+        },
+        data: resp.datas[key]
+    }))
+    myChart.value = echarts.init(chartDom.value, 'dark');
+    option && myChart.value.setOption(option);
+    return myChart
+}
 
 onMounted(async () => {
-    // // 获取 所有病害名称,并保存待用
-    // disCategory.value = await getAllDiseaseCategory()
-    // disNames.value = disCategory.value.map(value => {
-    //     return value.name
-    // })
-    // // 初始化图表数据, 调用绘制方法
-    // let mc = await initChart("week")
-    // let yc = await chartYear()
-    // // 解除加载状态
-    // loading.value = false
-    // // 记录数据更新时间
-    // dateTime.value = genDateTime()
-    //
-    // const visualContainer = document.querySelector("#visualContainer")
-    // // 创建 ResizeObserver 实例
-    // const resizeObserver = new ResizeObserver(() => {
-    //     // 当容器尺寸变化时，重绘制图表
-    //     mc.resize();
-    //     yc.resize();
-    // });
-    //
-    // // 观察 visualContainer 元素
-    // resizeObserver.observe(visualContainer);
+    await nextTick(async() => {
+        await chartYear()
+    })
+
 })
-// todo: 1.未作限流功能,可以持久化数据到客户端的某个地方,下次请求时优先使用客户端存在的数据,每三十分钟更新一次
-// todo: 2.每次都会重新初始化渲染图表dom, 待优化方案(深度合并-> 解决配置项多的嵌套配置)
-// todo: 3.当前组件或许需要拆分
 </script>
 
 <style scoped>
